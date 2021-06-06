45ba7f844f7c90b85872f8ee5a51def0b87ebb8e
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-06-06 14:11:26 -0700

    update

--- clustering_four_component.c
+++ clustering_four_component.c
@@ -1,3 +1,4 @@
+#include<math.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -34,6 +35,86 @@ const char* D_fn[] = {"pd_f.bin",
                       "pv_f.bin",
                       "pc_f.bin"};
 
+float max_f(float a, float b, float c){
+  return fmax(fmax(a,b), fmax(b,c));
+}
+float min_f(float a, float b, float c){
+  return fmin(fmin(a,b), fmin(b,c));
+}
+
+
+
+int rgb_to_hsv(float * src_r, float * src_g, float * src_b, float * dst_h, float * dst_s, float * dst_v){
+   // r, g, b, \in [0, 255]
+    float r = (*src_r) / 255.;
+    float g = (*src_g) / 255.;
+    float b = (*src_b) / 255.; // h:0-360.0, s:0.0-1.0, v:0.0-1.0
+
+    float max = max_f(r, g, b);
+    float min = min_f(r, g, b);
+    float h, s, v;
+    v = max;
+
+    if (max == 0.0f || (max - min == 0.)){
+      s = h = 0.;
+    }
+    else {
+        s = (max - min) / max;
+        if (max == r) h = 60 * ((g - b) / (max - min)) + 0;
+        else if (max == g) h = 60 * ((b - r) / (max - min)) + 120;
+        else h = 60 * ((r - g) / (max - min)) + 240;
+    }
+    if (h < 0) h += 360.;
+    *dst_h = h;   // dst_h : 0-180
+    *dst_s = s * 255.; // dst_s : 0-255
+    *dst_v = v * 255.; // dst_v : 0-255
+    return 0;
+}
+
+int hsv_to_rgb(float *r, float *g, float *b, float h, float s, float v){
+  // h \in [0, 360]. s, v \in [0, 1]
+  if( (h>360.)||(h<0.)){
+    printf("H: HSV out of range %f %f %f\n", h, s, v);
+    return(1);
+  }
+  if((s<0.)||(s>1.)){
+    printf("S: HSV out of range %f %f %f\n", h, s, v);
+    return(1);
+  }
+  if((v<0.)||(v>1.)){
+    printf("V: HSV out of range %f %f %f\n", h, s, v);
+    return(1);
+  }
+  if (h==360.){
+    h=0;
+  }
+  int i;
+  float f, p, q, t;
+  if( s == 0 ) {
+    // achromatic (grey)
+    *r = *g = *b = v;
+    return 0;
+  }
+  float H,S,V;
+  H=h; V=v; S=s;
+  h /= 60.; // sector 0 to 5
+  i = (int)floor( h );
+  f = h - i; // factorial part of h
+  p = v * ( 1. - s );
+  q = v * ( 1. - s * f );
+  t = v * ( 1. - s * ( 1 - f ) );
+  switch( i ) {
+    case 0: *r = v; *g = t; *b = p; break;
+    case 1: *r = q; *g = v; *b = p; break;
+    case 2: *r = p; *g = v; *b = t; break;
+    case 3: *r = p; *g = q; *b = v; break;
+    case 4: *r = t; *g = p; *b = v; break;
+    case 5: *r = v; *g = p; *b = q; break;
+    default: printf("\nERROR HSV to RGB"); printf("i=%d hsv= %f %f %f\n", i, H, S, V);
+    //exit(1);
+  }
+  return 0;
+}
 
 int main(int argc, char ** argv){
   if(argc < 2) err("clustering_four_component.c [path used to call MF4CF.c e.g. T3/]\n\te.g.:\n\t./clustering_four_component.exe T3");
@@ -59,7 +140,6 @@ int main(int argc, char ** argv){
   
   path_cat(fn, path, "MF4C_Clustered.bin");
   FILE * out = fopen(fn, "wb");
-
   path_cat(fn, path, "r.bin");
   FILE * out_r = fopen(fn, "wb");
   path_cat(fn, path, "g.bin");
@@ -99,22 +179,22 @@ int main(int argc, char ** argv){
     if(Pc > Pv && Pv > Pd && Pd > Ps) class = 23;
     if(Pc > Pv && Pv > Ps && Ps > Pd) class = 24;
 
-
-
     fwrite(&class, sizeof(float), 1, out);
 
     r = g = b = 0;
-    if((int)(class / 6) == 0) r = 254;
-    if((int)(class / 6) == 1) b = 254;
-    if((int)(class / 6) == 2) g = 254;
+    if((int)(class / 6) == 0) r = 255;
+    if((int)(class / 6) == 1) b = 255;
+    if((int)(class / 6) == 2) g = 255;
     if((int)(class / 6) == 3){
-        r = 254;
-        b = 254;
+        r = 255;
+        b = 255;
     }
 
-    r /= ((float)(((int)class) % 6));
-    g /= ((float)(((int)class) % 6));
-    b /= ((float)(((int)class) % 6));
+    float h, s, v;
+    rgb_to_hsv(&r,&g,&b,&h,&s,&v);
+    s *= (6. -  (((int)class) % 6)) / 6.;
+    hsv_to_rgb(&r, &g, &b, h, s/255., v/255.);
+
 
     fwrite(&r, sizeof(float), 1, out_r);
     fwrite(&g, sizeof(float), 1, out_g);
